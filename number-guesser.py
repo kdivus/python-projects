99e99c2894c063e4d8af205de93f9b2ba9475ccc
Krešimir Divić <kdivus@hotmail.com>
2021-01-12 16:30:58 +0100

    number guesser update

--- number-guesser.py
+++ number-guesser.py
@@ -2,7 +2,7 @@ import random
 
 print("Let's play a game...")
 
-def guess(x):
+""" def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
@@ -12,7 +12,23 @@ def guess(x):
         elif guess > random_number:
             print('Sorry, too high. Guess again!')
 
-    print(f'Congrats! You have guessed the number! The answer is number {random_number} !')           
+    print(f'Congrats! You have guessed the number! The answer is number {random_number} !')    """        
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1    
+    print(f'The computer guessed your number correctly. The answer is {guess}')        
 
-guess(10)    
+#guess(10)    
+computer_guess(300)
